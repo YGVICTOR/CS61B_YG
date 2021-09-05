c6a32c0c6f11c9b8cd0f7c02107f390227440cd2
YGVICTOR <535849296@qq.com>
2021-09-05 11:30:48 +0800

    change G to private to pass API check!

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -5,7 +5,7 @@ public class Planet{
 	public double yyVel;//Its current velocity in the y direction;
 	public double mass;//Its mass;
 	public String imgFileName;//The name of the file that corresponds to the image that depicts the planet
-	public static final double G = 6.67*Math.pow(10,-11);
+	private static final double G = 6.67*Math.pow(10,-11);
 
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img){
