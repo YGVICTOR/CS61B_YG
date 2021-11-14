ea88f364c69003c9802f3fdd6d15d21b1c1a3d45
YGVICTOR <535849296@qq.com>
2021-11-14 14:50:09 +0000

    first commit

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +11,58 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Maze maze;
+    private Queue<Integer> fringe;
+    private boolean targetFound = false;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX,sourceY);
+        t = maze.xyTo1D(targetX,targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        fringe = new Queue<>();
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int v) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        fringe.enqueue(v);
+        marked[v] = true;
+        announce();
+
+        while(!fringe.isEmpty()){
+            if(v==t){
+                targetFound = true;
+            }
+            if(targetFound){
+                return;
+            }
+            int out = fringe.dequeue();
+            for(int w:maze.adj(out)){
+                if(!marked[w]){
+                    fringe.enqueue(w);
+                    marked[w] = true;
+                    edgeTo[w] = out;
+                    distTo[w] = distTo[out] +1;
+                    announce();
+                    if(v==t){
+                        targetFound = true;
+                    }
+                    if(targetFound){
+                        return;
+                    }
+                }
+            }
+        }
     }
-
-
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,14 +11,66 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private class Node{
+        int current;
+        int source;
+        Node(int current,int source){
+            this.current = current;
+            this.source = source;
+        }
+    }
+
+    private Maze maze;
+    private Stack<Integer> stack = new Stack<>();
+    private boolean hasCircle;
+    private boolean overlap;
+    private int repeatPoint;
+
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(new Node(0,0));
+
+        while (!stack.isEmpty()){
+            int top = stack.pop();
+            if(stack.isEmpty()){
+                break;
+            }
+            edgeTo[stack.peek()] = top;
+            announce();
+        }
+    }
+    private void dfs(Node node){
+        marked[node.current] = true;
+        announce();
+        for(int w : maze.adj(node.current)){
+            if(marked[w] && node.source != w){
+                hasCircle = true;
+                stack.push(w);
+                repeatPoint = w;
+                overlap = false;
+                return;
+            }
+            if(!marked[w]){
+                Node newStart = new Node(w,node.current);
+                dfs(newStart);
+            }
+            if(hasCircle){
+                if(!overlap){
+                    stack.push(w);
+                }
+                if(w == repeatPoint){
+                    overlap = true;
+                }
+                return;
+            }
+        }
     }
 
     // Helper methods go here
